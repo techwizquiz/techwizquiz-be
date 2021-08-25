f198681fbc47635d1e15c4c382ee6869ff593170
cmmerritt <christiane.merritt@gmail.com>
2021-08-25 13:30:57 -0700

    add auth test - post user to signup

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,24 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('auth routes', () => {
+  const user = {
+    username: 'peaches',
+    password: 'peachesthedog'
+  };
+
   beforeEach(() => {
     return setup(pool);
+  });
+
+  test('POST user to /auth/signup', async () => {
+    const res = await request(app)
+      .post('/api/v1/auth/signup')
+      .send(user);
+    expect(res.body).toEqual({
+      id: '1',
+      username: user.username,
+      passwordHash: expect.any(String)
+    });
   });
 });

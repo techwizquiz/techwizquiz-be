3d904b89d3f18e948fe3cb36484b974ec5e6f36d
cmmerritt <christiane.merritt@gmail.com>
2021-08-26 21:31:14 -0700

    add more question data

--- data/questions.js
+++ data/questions.js
@@ -148,5 +148,210 @@ export default [
   `,
     language: 'JavaScript'
   },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output?',
+    questionText: `
+    let a = 3;
+    let b = new Number(3);
+    let c = 3;
+    
+    console.log(a == b);
+    console.log(a === b);
+    console.log(b === c);
+    `,
+    answer: 'c',
+    a: 'true false true',
+    b: 'false false true',
+    c: 'true false false',
+    d: 'false true true',
+    explanation: `
+    new Number() is a built-in function constructor. Although it looks like a number, it's not really a number: it has a bunch of extra features and is an object.
+
+    When we use the == operator, it only checks whether it has the same value. They both have the value of 3, so it returns true.
+
+    However, when we use the === operator, both value and type should be the same. It's not: new Number() is not a number, it's an object. Both return false.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output?',
+    questionText: `
+    class Chameleon {
+      static colorChange(newColor) {
+        this.newColor = newColor;
+        return this.newColor;
+      }
+    
+      constructor({ newColor = 'green' } = {}) {
+        this.newColor = newColor;
+      }
+    }
+    
+    const freddie = new Chameleon({ newColor: 'purple' });
+    console.log(freddie.colorChange('orange'));
+    `,
+    answer: 'd',
+    a: 'orange',
+    b: 'purple',
+    c: 'green',
+    d: 'TypeError',
+    explanation: `
+    The colorChange function is static. Static methods are designed to live only on the constructor in which they are created, and cannot be passed down to any children. Since freddie is a child, the function is not passed down, and not available on the freddie instance: a TypeError is thrown.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output?',
+    questionText: `
+    let greeting;
+    greetign = {}; // Typo!
+    console.log(greetign);
+    `,
+    answer: 'a',
+    a: '{}',
+    b: 'ReferenceError: greetign is not defined',
+    c: 'undefined',
+    d: 'greetign',
+    explanation: `
+    It logs the object, because we just created an empty object on the global object! When we mistyped greeting as greetign, the JS interpreter actually saw this as global.greetign = {} (or window.greetign = {} in a browser).
+
+    In order to avoid this, we can use "use strict". This makes sure that you have declared a variable before setting it equal to anything.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What happens when we do this?',
+    questionText: `
+    function bark() {
+      console.log('Woof!');
+    }
+    
+    bark.animal = 'dog';
+    `,
+    answer: 'a',
+    a: 'Nothing, this is totally fine!',
+    b: 'SyntaxError. You cannot add properties to a function this way',
+    c: '"Woof" gets logged',
+    d: 'ReferenceError',
+    explanation: `
+    This is possible in JavaScript, because functions are objects! (Everything besides primitive types are objects)
+
+    A function is a special type of object. The code you write yourself isn't the actual function. The function is an object with properties. This property is invocable.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output??',
+    questionText: `
+    function Person(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    const member = new Person('Lydia', 'Hallie');
+    Person.getFullName = function() {
+      return \`${this.firstName} ${this.lastName}\`;
+    };
+    
+    console.log(member.getFullName());
+    `,
+    answer: 'a',
+    a: 'TypeError',
+    b: 'SyntaxError',
+    c: 'Lydia Hallie',
+    d: 'undefined undefined',
+    explanation: `
+    In JavaScript, functions are objects, and therefore, the method getFullName gets added to the constructor function object itself. For that reason, we can call Person.getFullName(), but member.getFullName throws a TypeError.
+
+    If you want a method to be available to all object instances, you have to add it to the prototype property:
+
+    Person.prototype.getFullName = function() {
+      return \`${this.firstName} ${this.lastName}\`;
+    };
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output?',
+    questionText: `
+    function Person(firstName, lastName) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+    }
+    
+    const lydia = new Person('Lydia', 'Hallie');
+    const sarah = Person('Sarah', 'Smith');
+    
+    console.log(lydia);
+    console.log(sarah);
+    `,
+    answer: 'a',
+    a: 'Person {firstName: "Lydia", lastName: "Hallie"} and undefined',
+    b: 'Person {firstName: "Lydia", lastName: "Hallie"} and Person {firstName: "Sarah", lastName: "Smith"}',
+    c: 'Person {firstName: "Lydia", lastName: "Hallie"} and {}',
+    d: 'Person {firstName: "Lydia", lastName: "Hallie"} and ReferenceError',
+    explanation: `
+    For sarah, we didn't use the new keyword. When using new, this refers to the new empty object we create. However, if you don't add new, this refers to the global object!
+
+    We said that this.firstName equals "Sarah" and this.lastName equals "Smith". What we actually did, is defining global.firstName = 'Sarah' and global.lastName = 'Smith'. sarah itself is left undefined, since we don't return a value from the Person function.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'Select the best answer',
+    questionText: 'What are the three phases of event propagation?',
+    answer: 'd',
+    a: 'Target > Capturing > Bubbling',
+    b: 'Bubbling > Target > Capturing',
+    c: 'Target > Bubbling > Capturing',
+    d: 'Capturing > Target > Bubbling',
+    explanation: `
+    During the capturing phase, the event goes through the ancestor elements down to the target element. It then reaches the target element, and bubbling begins.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'Select the best answer',
+    questionText: 'All objects have prototypes.',
+    answer: 'b',
+    a: 'True',
+    b: 'False',
+    c: 'There are no objects in JavaScript',
+    d: 'No JavaScript objects have prototypes',
+    explanation: `
+    All objects have prototypes, except for the base object. The base object is the object created by the user, or an object that is created using the new keyword. The base object has access to some methods and properties, such as .toString. This is the reason why you can use built-in JavaScript methods! All of such methods are available on the prototype. Although JavaScript can't find it directly on your object, it goes down the prototype chain and finds it there, which makes it accessible for you.
+  `,
+    language: 'JavaScript'
+  },
+  {
+    level: 1,
+    questionTitle: 'What\'s the output?',
+    questionText: `
+    function sum(a, b) {
+      return a + b;
+    }
+    
+    sum(1, '2');
+    `,
+    answer: 'c',
+    a: 'NaN',
+    b: 'TypeError',
+    c: '"12"',
+    d: '3',
+    explanation: `
+    JavaScript is a dynamically typed language: we don't specify what types certain variables are. Values can automatically be converted into another type without you knowing, which is called implicit type coercion. Coercion is converting from one type into another.
+
+    In this example, JavaScript converts the number 1 into a string, in order for the function to make sense and   return a value. During the addition of a numeric type (1) and a string type ('2'), the number is treated as a  string. We can concatenate strings like "Hello" + "World", so what's happening here is "1" + "2" which returns "12".
+  `,
+    language: 'JavaScript'
+  },
 ];
 

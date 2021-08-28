fcd7b470ca6157285de0e29b6b52f65ecdc2d66e
cmmerritt <christiane.merritt@gmail.com>
2021-08-27 17:08:30 -0700

    post response test passes

--- __tests__/responses.test.js
+++ __tests__/responses.test.js
@@ -2,11 +2,31 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import UserService from '../lib/services/UserService.js';
+import Question from '../lib/models/Question.js';
 
 const response1 = {
-  user_id: 1,
-  question_id: 7,
-  correct: true
+  userId: '1',
+  questionId: '1',
+  isCorrect: true
+};
+
+const user = {
+  email: 'peachy@peachy.com',
+  password: 'peachesthedog'
+};
+
+const peachesQ = {
+  level: 2,
+  questionTitle: 'Answer',
+  questionText: 'Does Peaches have a dog\'s name?',
+  answer: 'a',
+  a: 'yes',
+  b: 'no',
+  c: 'maybe',
+  d: 'actually a cat',
+  explanation: 'it is obvious',
+  language: 'JavaScript'
 };
 
 describe('responses routes', () => {
@@ -15,8 +35,11 @@ describe('responses routes', () => {
   });
 
   it('creates a response via POST', async () => {
+    await UserService.create(user);
+    await Question.insert(peachesQ);
+
     const res = await request(app)
-      .post('/api/v1/questions')
+      .post('/api/v1/responses')
       .send(response1);
     expect(res.body).toEqual({
       responseId: '1',

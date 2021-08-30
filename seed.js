0fa9bc6fb777abe255b00417691dce9df6471122
cmmerritt <christiane.merritt@gmail.com>
2021-08-30 11:32:44 -0700

    add script to seed questions to db

---
+++ seed.js
@@ -0,0 +1,19 @@
+import Question from './lib/models/Question.js';
+import { questions } from './data/questions.js';
+// import pool from './lib/utils/pool.js';
+
+run();
+
+async function run() {
+  try {
+    await Promise.all(
+      questions.map(question => {
+        Question.insert(question);
+      })
+    );
+    console.log('seed data load complete');
+  }
+  catch(err) {
+    console.log(err);
+  }
+}
